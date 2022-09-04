3b8a02f1fd0171d8dd023a580b324aa910532189
Nirmal Bera <57650432+LazyCoder-N@users.noreply.github.com>
2022-09-04 13:37:06 +0530

    Update incyubyte_test.py

--- incyubyte_test.py
+++ incyubyte_test.py
@@ -2,11 +2,18 @@ def add():
 
 	user_input = input("Enter Numbers: ")
 	addition = 0
+	addition_char = 0
 
 	for i in user_input:
+
 		if i.isnumeric():
 			addition += int(i)
 
-	print(addition)
+		else:
+			val = ord(i)
+			val_num = val - 96
+			addition_char = addition + val_num
+	
+	print(addition_char)
 
 add()
