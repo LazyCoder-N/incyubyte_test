77cb558d543ae8b057c76a68ac3196d021ce090a
Nirmal Bera <57650432+LazyCoder-N@users.noreply.github.com>
2022-09-04 15:28:24 +0530

    Update incyubyte_test.py

--- incyubyte_test.py
+++ incyubyte_test.py
@@ -2,20 +2,25 @@ def add():
 
 	user_input = input("Enter Numbers: ")
 	user_in_list = user_input.split(',')
+	print(user_in_list)
 	addition = 0
 	addition_char = 0
 	val_sum = 0
 	for i in user_in_list:
 
-		if i.isnumeric():
-			addition += int(i)
+		if i.isdigit():
+			int_num = int(i)
 
-		else:
+			if int_num < 1000:
+				addition += int_num
+
+		else :
 			val = ord(i)
 			val_num = val - 96
 			val_sum += val_num
-			addition_char = addition + val_sum
-	
+
+		addition_char = addition + val_sum
+
 	print(addition_char)
 
 add()
